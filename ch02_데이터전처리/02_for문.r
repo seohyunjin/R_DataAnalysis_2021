0dd4f831020e4c35ec732885ab58ecf50e3af0a4
hyunjin <hjhy0513@gmail.com>
2021-06-11 23:36:31 +0900

    0611

---
+++ ch02_데이터전처리/02_for문.r
@@ -0,0 +1,7 @@
+# 0611
+# for(변수 in 데이터){
+#    반복할 코드
+#}
+for(year in c(2015:2018)){
+    print(paste('The year is',year))
+}
---
+++ ch02_데이터전처리/03_while문.r
@@ -0,0 +1,11 @@
+#변수 <- 초기값
+
+# while(조건){
+#     조건이 참일때 수행할 코드
+# }
+
+year <- 2015
+while(year<=2018){
+    print(pasete("The year is",year))
+    year <- year+1
+}
